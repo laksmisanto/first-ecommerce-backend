187ef81fefad97bc5068426b6182de0b6fdfdadd
laksmi santo <84493741+laksmisanto@users.noreply.github.com>
2023-12-06 00:28:40 +0600

    save-data-form-database

--- controller/registrationController.js
+++ controller/registrationController.js
@@ -1,6 +1,7 @@
 const nameValidation = require("../helpers/nameValidation");
 const emailValidation = require("../helpers/emailValidation");
 const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 async function registrationController(req, res) {
   try {
@@ -29,10 +30,40 @@ async function registrationController(req, res) {
     }
     if (!emailValidation(email)) {
       return res.status(400).send({
-        error: "email Is Not Valid",
+        error: "Email Is Not Valid",
       });
     }
     //password bcrypt
+
+    let exitingMail = await User.find({ email });
+
+    if (exitingMail.length > 0) {
+      return res.status(400).send({
+        error: "Email already exists",
+      });
+    }
+
+    bcrypt.hash(password, 10, async function (err, hash) {
+      let userData = new User({
+        firstName,
+        lastName,
+        email,
+        password: hash,
+        telephone,
+        address,
+        city,
+        postCode,
+        country,
+        state,
+      });
+      userData.save();
+      res.json({
+        success: "Registration successfull",
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        email: userData.email,
+      });
+    });
 
     res.send(req.body);
   } catch (error) {
